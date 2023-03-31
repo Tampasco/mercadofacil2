f9ded15bf7260828f0223d42d61827938a4b8248
Tampasco <lucas.carvalho@ccc.ufcg.edu.br>
2023-03-30 21:42:11 -0300

    Pratica 3 inicio

---
+++ src/main/java/com/ufcg/psoft/mercadofacil/controller/ProdutoV1Controller.java
@@ -0,0 +1,5 @@
+package com.ufcg.psoft.mercadofacil.controller;
+
+public class ProdutoV1Controller {
+    
+}
---
+++ src/test/java/com/ufcg/psoft/controller/ProdutoV1ControllerTest.java
@@ -0,0 +1,8 @@
+package com.ufcg.psoft.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProdutoV1ControllerTest {
+
+}
